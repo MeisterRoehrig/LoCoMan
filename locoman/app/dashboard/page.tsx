e0d44b883783926302efb6688b116fe06a6263d8
MeisterRoehrig <33903777+MeisterRoehrig@users.noreply.github.com>
2025-03-10 10:08:42 +0100

    Firbase

---
+++ locoman/app/dashboard/page.tsx
@@ -0,0 +1,192 @@
+"use client"
+
+import { AppSidebar } from "@/components/app-sidebar"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
+import {
+  SidebarInset,
+  SidebarMenuButton,
+  SidebarProvider,
+  SidebarTrigger
+} from "@/components/ui/sidebar"
+import { Input } from "@/components/ui/input"
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable"
+
+import { ScrollArea } from "@/components/ui/scroll-area"
+import { Button } from "@/components/ui/button"
+import { buttonVariants } from "@/components/ui/button"
+
+import Link from "next/link"
+
+import { TreeView, TreeDataItem } from '@/components/ui/tree-view';
+import { FolderOpen, FolderClosed, File, FilePen } from "lucide-react"
+import { CirclePlus } from "lucide-react";
+import { ScrollDiv } from "@/components/ui/scroll-div"
+
+const data: TreeDataItem[] = [
+  {
+    id: '0',
+    name: 'Beauftragung',
+    actions: <div><Link className={buttonVariants({ variant: "ghost" })} href={"javascript:void(0)"} onClick={() => console.log("confirmed")}><CirclePlus/></Link></div>,
+    
+    children: [
+      {
+        id: '1',
+        name: 'Transportbedarf überprüfen',
+      },
+      {
+        id: '2',
+        name: 'Angebot vorbereiten und übermitteln',
+      },
+      {
+        id: '3',
+        name: 'Transportbedarf erfassen',
+      },
+    ],
+  },
+  {
+    id: '4',
+    name: 'Disposition',
+    children: [
+      {
+        id: '5',
+        name: 'Verfügbarkeit von Personal und Fuhrpark prüfen',
+      },
+      {
+        id: '6',
+        name: 'Tour planen',
+      },
+      {
+        id: '7',
+        name: 'Touren, Fahrzeuge und Fahrer disponieren',
+      },
+      {
+        id: '8',
+        name: 'Frachtpapiere vorbereiten',
+      },
+      {
+        id: '9',
+        name: 'Dokumente an Fahrer übermitteln',
+      },
+    ],
+  },
+  {
+    id: '10',
+    name: 'Beladung',
+    children: [
+      {
+        id: '11',
+        name: 'Zum Abholort fahren',
+      },
+      {
+        id: '12',
+        name: 'Verladepapiere an Erfassungsstelle übergeben',
+      },
+      {
+        id: '13',
+        name: 'Beladeplatz zuweisen',
+      },
+      {
+        id: '14',
+        name: 'Sendung überprüfen',
+      },
+      {
+        id: '15',
+        name: 'Sendung beladen',
+      },
+    ],
+  },
+  {
+    id: '16',
+    name: 'Transport',
+    children: [
+      {
+        id: '17',
+        name: 'Fahrerkarte einlesen',
+      },
+      {
+        id: '18',
+        name: 'Fahrzeugzustand prüfen und vorbereiten',
+      },
+      {
+        id: '19',
+        name: 'sendung transportieren',
+      },
+    ],
+  },
+  {
+    id: '20',
+    name: 'Entladung',
+    children: [
+      {
+        id: '21',
+        name: 'Sendung entladen',
+      },
+      {
+        id: '22',
+        name: 'Sendung überprüfen',
+      },
+      {
+        id: '23',
+        name: 'Sendung annehmen',
+      },
+    ],
+  },
+];
+
+export default function Page() {
+  return (
+    <SidebarProvider className="h-screen p-0 m-0" >
+      <AppSidebar />
+      <SidebarInset >
+        <header className="flex h-16 shrink-0 items-center gap-2">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">
+                    Neues Projekt
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Dateneingabe</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+
+        <ResizablePanelGroup
+          direction="horizontal"
+          className="flex-1 flex-col">
+          <ResizablePanel defaultSize={25}>
+            <ScrollDiv className="h-full">
+              <TreeView className=" p-4 pt-0" data={data} initialSelectedItemId="0" expandAll={true} />
+            </ScrollDiv>
+          </ResizablePanel>
+          {<ResizableHandle withHandle={false} />}
+          <ResizablePanel defaultSize={75}>
+            <ScrollArea type="scroll" className="flex-1 p-4 pt-0 rounded-md h-full">
+              <span className="font-semibold">Sidebar</span>
+            </ScrollArea>
+          </ResizablePanel>
+        </ResizablePanelGroup>
+
+      </SidebarInset>
+    </SidebarProvider>
+  )
+}
--- locoman/app/layout.tsx
+++ locoman/app/layout.tsx
@@ -2,6 +2,11 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
+import { Toaster } from "@/components/ui/sonner"
+import "@/app/globals.css";
+import { cn } from "@/lib/utils";
+import { MyFirebaseProvider } from "@/components/firebase-providers";
+import { ReactNode } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,14 +33,16 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange>
+          <MyFirebaseProvider>
             {children}
-          </ThemeProvider>
+            <Toaster />
+          </MyFirebaseProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
