18e82aa9ee995faadf831dffb5fa2feeaa91dad7
MeisterRoehrig <33903777+MeisterRoehrig@users.noreply.github.com>
2025-03-26 21:30:32 +0100

    WebHostingSetup

--- locoman/app/layout.tsx
+++ locoman/app/layout.tsx
@@ -1,10 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider"
-import { Toaster } from "@/components/ui/sonner"
-import "@/app/globals.css";
-import { MyFirebaseProvider } from "@/components/firebase-providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,20 +23,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange>
-          <MyFirebaseProvider>
-            {children}
-            <Toaster />
-          </MyFirebaseProvider>
-        </ThemeProvider>
+        {children}
       </body>
     </html>
   );
